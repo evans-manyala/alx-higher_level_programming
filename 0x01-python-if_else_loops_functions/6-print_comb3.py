7c13fe9be75bc1a048fddb6acd5b4f834e7061df
evans-manyala <evans.manyala@gmail.com>
2023-11-29 11:01:55 +0300

    Fixing logical errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(x + 1, 10):
-        print(f"{x}{y}, ", end="")
+for x in range(1, 89):
+    if x < 10 or x // 10 < x % 10:
+        print(f"{x:02d}", end=", ")
+print(89)
