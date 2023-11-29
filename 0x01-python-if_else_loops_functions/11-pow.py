fd43808ce1eb0d17c7f6f1d1f765af1efa28b3e9
evans-manyala <evans.manyala@gmail.com>
2023-11-29 15:32:33 +0300

    Fixing logical error in code

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -7,10 +7,10 @@ def pow(a, b):
     if a < 0:
         neg = True
         a = abs(a)
-    for x in range(b):
-        if neg:
-            if b % 2 == 1:
-                result *= -1
-            neg = False
+    for x in range(abs(b)):
         result *= a
+    if b < 0:
+        result = 1 / result
+    if neg:
+        result *= -1
     return result
