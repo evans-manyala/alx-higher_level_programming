48c442ed9e5f7081bfc5289f764ebdf3795e9337
evans-manyala <evans.manyala@gmail.com>
2023-11-29 12:08:49 +0300

    Removing logical errors in code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,6 @@ def uppercase(str):
     for letter in str:
         if letter >= 'a' and letter <= 'z':
             letter = chr(ord(letter) - 32)
-        print("{}".format(letter), end='')
-    print
+        else:
+            print("{}".format(letter), end='')
+    print()
