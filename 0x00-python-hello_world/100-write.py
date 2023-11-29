82af29f6526e99392eb661e63ca3a9c5e6e02476
evans-manyala <evans.manyala@gmail.com>
2023-11-30 00:21:09 +0300

    Adding functional code

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19, \n")
-sys.exit(1)
+#!/usr/bin/python3
+from sys import stderr, exit
+if __name__ == "__main__":
+    stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19, \n")
+    exit(1)
