2f9abc2429fb50a64c9ed334e47debb53e85b868
evans-manyala <evans.manyala@gmail.com>
2023-11-29 13:55:55 +0300

    Adding logical error fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        if letter.islower():
+        if letter >= 97 and letter <= 122:
             letter = chr(ord(letter) - 32)
         print("{}".format(letter), end='')
     print()
+    
