db6b8a2f6d0a5b365c3a8f7f17cdd926808df688
evans-manyala <evans.manyala@gmail.com>
2023-11-30 00:23:07 +0300

    Fixing pycodestyle error

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from sys import stderr, exit
 if __name__ == "__main__":
-    stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19, \n")
+    stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
     exit(1)
