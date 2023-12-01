31553620a55cb5b7a3e506e8fc7a486bf4eeb0ac
Evans Manyala <evans.manyala@gmail.com>
2023-12-01 10:29:18 +0300

    Fixing logical and syntax errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for arg in sys.argv[1:]:
-    sum += int(arg)
-print(sum)
+if __name__ == "__main__":
+    import sys
+
+    sum, x = 0, 0
+    for arg in sys.argv:
+        if (x > 0):
+            sum += int(arg)
+        x += 1
+        print(sum)
