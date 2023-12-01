1b758308cbbaa69403a8c6b033ec8f84dddb2f07
Evans Manyala <evans.manyala@gmail.com>
2023-12-01 10:36:27 +0300

    Adding logical error fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    sum, x = 0, 0
-    for arg in sys.argv:
-        if (x > 0):
-            sum += int(arg)
-        x += 1
+    sum = 0
+    for arg in sys.argv[1:]:
+        sum += int(arg)
         print(sum)
