e226bb35a801973fecdcdc698e68298f05c4240d
evans-manyala <evans.manyala@gmail.com>
2023-12-01 13:37:01 +0300

    Fixing code logic error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    name = dir(hidden_4)
-    for x in name:
-        if x[:2] != "__":
-            print("{:s}".format(x))
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != "__":
+            print(name)
