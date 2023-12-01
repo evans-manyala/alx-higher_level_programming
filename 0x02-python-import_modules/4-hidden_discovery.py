cf61efdbcc2c4d9053b82415cb6f4c115f4cba78
Evans Manyala <evans.manyala@gmail.com>
2023-12-01 11:09:15 +0300

    Fixing logical errors in the script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for x in dir(hidden_4):
+    name = dir(hidden_4)
+    for x in name:
         if x[:2] != "__":
             print("{:s}".format(x))
