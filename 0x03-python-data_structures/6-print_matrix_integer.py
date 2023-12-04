3078f777d8ef4dc694dc4bda100e542f414af788
evans-manyala <evans.manyala@gmail.com>
2023-12-04 18:08:28 +0300

    Logic failure fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for integer in row:
-            print("{:d}".format(integer), end=" ")
-        print()
+    for row in range(len(matrix)):
+        for integer in range(len(matrix[row]) - 1):
+            print("{:d}".format(matrix[row][integer]), end=' ')
+        if matrix[row]:
+            print("{:d}".format(matrix[row][-1]))
+        else:
+            print()
