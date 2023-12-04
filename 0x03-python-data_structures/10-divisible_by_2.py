8c8c324c250bc726983f7d2dda7415639684b56b
Evans Manyala <evans.manyala@gmail.com>
2023-12-05 00:46:02 +0300

    Error fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+
     final_list = []
-    for num in my_list:
-            final_list.append(True if num % 2 == 0 else False)
+
+    for num in range(len(my_list)):
+        final_list.append(True if my_list[num] % 2 == 0 else False)
+
     return final_list
