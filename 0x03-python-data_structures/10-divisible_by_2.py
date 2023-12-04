90c35bda64fec59b4b945c68ac0f4662e19542d3
Evans Manyala <evans.manyala@gmail.com>
2023-12-05 00:16:16 +0300

    Fixing logic in script

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,8 @@
 def divisible_by_2(my_list=[]):
     final_list = []
     for num in my_list:
-            final_list.append(True if num % 2 == 0 else False)
+        if my_list[num] % 2 == 0:
+            final_list.append(True)
+        else:
+            final_list.append(False)
     return final_list
-    
