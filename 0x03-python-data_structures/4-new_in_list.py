455479a9650715b9465197003c967484bfac707b
evans-manyala <evans.manyala@gmail.com>
2023-12-04 17:14:50 +0300

    Logical error fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,4 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = element
+        new_list = my_list
+        new_in_list[idx] = element
+        return new_list
