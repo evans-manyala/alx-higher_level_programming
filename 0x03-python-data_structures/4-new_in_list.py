39948ba659c5eae58a3e404a1eeff6644854df55
evans-manyala <evans.manyala@gmail.com>
2023-12-04 17:23:13 +0300

    Adding logic error fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        new_list = my_list
-        new_in_list[idx] = element
+    if 0 <= idx or idx >= len(my_list):
+        new_list = []
+        x = 0
+        for number in my_list:
+            if x != idx:
+                new_list.append(number)
+            else:
+                new_list.append(element)
+            x = x + 1
         return new_list
+    return my_list
