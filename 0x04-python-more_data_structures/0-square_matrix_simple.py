1582b091255bd2376c3160765293234e8cb8fedc
evans-manyala <evans.manyala@gmail.com>
2023-12-06 14:35:47 +0300

    Initial commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in matrix:
+        squared_row = []
+        for element in row:
+            squared_value = element * element
+            squared_row.append(squared_value)
+        squared_matrix.append(squared_row)
+    return squared_matrix
