6a4768a020ebe237ba5ba4c6cdfd504c5f31ac64
Evans Manyala <evans.manyala@gmail.com>
2023-12-07 23:46:54 +0300

    Fixing script syntax errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
 
     if not isinstance(roman_string, str) or roman_string is None:
@@ -16,7 +17,8 @@ def roman_to_int(roman_string):
     result = 0
     for x, char in enumerate(roman_string):
         value = roman_numerals[char]
-        if x + 1 < len(roman_string) and value < roman_numerals[roman_string[x + 1]]:
+        if x + 1 < len(roman_string) and
+        value < roman_numerals[roman_string[x + 1]]:
             result -= value
         else:
             result += value
