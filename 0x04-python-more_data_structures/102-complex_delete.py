2adefcf8cabe1d74f1edbf4a0d5c502a380a3a3f
Evans Manyala <evans.manyala@gmail.com>
2023-12-08 00:40:38 +0300

    Pycodestyle error fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    items_to_delete = [key for key, val in a_dictionary.items() if val == value]
+    items_to_delete = [key for key, val in a_dictionary.items()
+                       if val == value]
     for key in items_to_delete:
         del a_dictionary[key]
     return a_dictionary
