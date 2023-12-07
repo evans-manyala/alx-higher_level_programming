bf6ec9fa8d76624928e75ff33470aeca211ad527
Evans Manyala <evans.manyala@gmail.com>
2023-12-07 23:45:18 +0300

    Fixing script errors in logic execution

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,23 @@
 def roman_to_int(roman_string):
 
-  if not isinstance(roman_string, str) or roman_string is None:
-    return 0
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
 
-  roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,}
+    roman_numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
 
-  result = 0
-  for i, char in enumerate(roman_string):
-    value = roman_numerals[char]
-    if i + 1 < len(roman_string) and value < roman_numerals[roman_string[i + 1]]:
-      result -= value
-    else:
-      result += value
-  return result
- 
+    result = 0
+    for x, char in enumerate(roman_string):
+        value = roman_numerals[char]
+        if x + 1 < len(roman_string) and value < roman_numerals[roman_string[x + 1]]:
+            result -= value
+        else:
+            result += value
+    return result
