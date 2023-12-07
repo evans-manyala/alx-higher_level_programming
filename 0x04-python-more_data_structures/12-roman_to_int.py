2d94eff98b21ea409888f262709bfdd660e34834
Evans Manyala <evans.manyala@gmail.com>
2023-12-07 23:41:53 +0300

    adding condition statements

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,16 @@
 def roman_to_int(roman_string):
-    
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,}    
-    result = 0
-    
-    for i, char in enumerate(roman_string):
-       value = roman_numerals[char]
-       
-       if i + 1 < len(roman_string) and value < roman_numerals[roman_string[i + 1]]:
-          result -= value
-        else:
-           result += value
-    return result
+
+  if not isinstance(roman_string, str) or roman_string is None:
+    return 0
+
+  roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,}
+
+  result = 0
+  for i, char in enumerate(roman_string):
+    value = roman_numerals[char]
+    if i + 1 < len(roman_string) and value < roman_numerals[roman_string[i + 1]]:
+      result -= value
+    else:
+      result += value
+  return result
+ 
