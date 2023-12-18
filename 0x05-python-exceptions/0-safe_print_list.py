92508df3b3dc8e5d62e2284fa5b5d7061881289e
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 12:11:46 +0300

    Changing how printout looks like

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], y=0):
     printed_items = 0
     try:
         for x in range(y):
-            print(my_list[x], end=" ")
+            print(my_list[x], end="")
             printed_items += 1
     except IndexError:
         pass
