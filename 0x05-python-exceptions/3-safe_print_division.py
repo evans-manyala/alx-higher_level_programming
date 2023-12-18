49a5ed30faf5f29ea57b035b448cb43b41a44cf5
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 18:48:06 +0300

    Adding erro fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result:", end=" ")
-        print("{:.2f}".format(result)) if result is not None else print(None)
-    return result
+         print('Inside result: {}'.format(result))
+         return result
