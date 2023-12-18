0f76a1a60b584f3792a9f7f817dab32deafd4e84
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 11:59:44 +0300

    Adding python script

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], y=0):
+    printed_items = 0
+    try:
+        for x in range(y):
+            print(my_list[x], end=" ")
+            printed_items += 1
+    except IndexError:
+        pass
+
+    print()
+    return printed_items
