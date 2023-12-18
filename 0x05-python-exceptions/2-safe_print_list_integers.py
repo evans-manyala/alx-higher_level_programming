8e7d2e15deb954c407afcd3939c1c272eec00f14
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 14:27:08 +0300

    Adding error fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[y], int):
                 print("{:d}".format(my_list[y]), end="")
                 printed_items += 1
-    except IndexError:
+    except (ValueError, TypeError):
         pass
     print()
     return printed_items
