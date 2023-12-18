a48bd0b930e1e4bae28be0728db5ff6c3b8d7ca1
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 13:09:16 +0300

    Adding python script

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_items = 0
+
+    if x > len(my_list):
+        raise IndexError("list index out of range")
+
+    try:
+        for y in range(x):
+            if isinstance(my_list[y], int):
+                print("{:d}".format(my_list[y]))
+                printed_items += 1
+    except IndexError:
+        pass
+    print()
+    return printed_items
