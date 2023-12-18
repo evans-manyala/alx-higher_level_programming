433c90599fd63b125aae39355dfef40c61cc89b7
Evans Manyala <evans.manyala@gmail.com>
2023-12-18 18:43:56 +0300

    Adding script error fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,19 +1,10 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
-  """
-  Attempts to divide two integers and print the result, handling various scenarios.
-
-  Args:
-      a: First integer operand.
-      b: Second integer operand (divisor).
-
-  Returns:
-      The result of the division if successful, otherwise None.
-  """
-  try:
-    result = a / b
-  except ZeroDivisionError:
-    result = None
-  finally:
-    print("Inside result:", end=" ")
-    print("{:.2f}".format(result)) if result is not None else print(None)
-  return result
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result:", end=" ")
+        print("{:.2f}".format(result)) if result is not None else print(None)
+    return result
