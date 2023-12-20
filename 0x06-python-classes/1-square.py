1df8208f8089ab8368e3e0155d2b5be1b4fddf71
Evans Manyala <evans.manyala@gmail.com>
2023-12-20 13:56:33 +0300

    Fixing pyscript error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
-
 class Square:
     def __init__(self, size):
         """Represents a square."""
-
-        self.size = size
+        self._sizesize = size
