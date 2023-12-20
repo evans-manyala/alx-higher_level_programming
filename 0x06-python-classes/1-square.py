5c2d74f45353247a07d774f44abb47908d7db75a
Evans Manyala <evans.manyala@gmail.com>
2023-12-20 14:03:15 +0300

    Fixing Pycodestyling error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     def __init__(self, size):
         """
         Represents a square
-       
+
         ""
         Args:
             size (Any type): the size of the square
