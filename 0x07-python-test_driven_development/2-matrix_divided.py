08b946d24174119b9df678f6b0a75129e0d91ff2
evans-manyala <evans.manyala@gmail.com>
2024-01-08 01:59:02 +0300

    Adding code snippets

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,3 +19,5 @@ def matrix_divided(matrix, div):
         has been divided by div and rounded to 2 decimal places.
 
     """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
