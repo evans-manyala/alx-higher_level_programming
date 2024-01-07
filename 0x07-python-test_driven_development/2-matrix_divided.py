1d5bd77caf84247b93dc3288aff84227d2a04c50
evans-manyala <evans.manyala@gmail.com>
2024-01-08 02:00:53 +0300

    Final code snippet

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -25,4 +25,7 @@ def matrix_divided(matrix, div):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    matr = [[round(elem / div, 2) for elem in row] for row in matrix]
+    return matr
