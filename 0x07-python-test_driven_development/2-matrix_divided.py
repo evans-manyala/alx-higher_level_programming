4cce132b38a27dcbe0e3e6490918ac7231cf5d0d
evans-manyala <evans.manyala@gmail.com>
2024-01-08 01:58:29 +0300

    Initial code commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by a number, rounding up to 2 decimal places.
+
+    Args:
+        matrix: A list of integers or floats representing the matrix.
+        div: The number to divide each element by.
+
+    Raises:
+        TypeError: If matrix is not a list of integers or floats, if the rows
+        of the matrix have different sizes, or if div is not a number.
+        ZeroDivisionError: If div is 0.
+    
+    Returns:
+        A new matrix with the same dimensions as the input matrix, where each element
+        has been divided by div and rounded to 2 decimal places.
+
+    """
