711edec836bc8fbb2d4be54cd48b4527e5953756
evans-manyala <evans.manyala@gmail.com>
2024-01-07 23:30:31 +0300

    Fixing Pycode style errors

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -63,7 +63,8 @@ class Rectangle:
     def __repr__(self):
         """Returns a string representation for recreating the rectangle."""
         return f"Rectangle({self.width}, {self.height})"
+
     def __del__(self):
         """Prints a message when rectangle instance is deleted"""
-        print ("Bye Rectangle...")
+        print("Bye Rectangle...")
         Rectangle.number_of_instancss -= 1
