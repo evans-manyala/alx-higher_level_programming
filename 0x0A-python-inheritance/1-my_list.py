5dc80aa6a60e7a6626cda7ea362fe6dcca4ff263
evans-manyala <evans.manyala@gmail.com>
2024-01-09 02:00:48 +0300

    Adding code commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Class MyList that inherits from list"""
 
+class MyList(list):
+    """This class inherits from list class"""
+    
+    def print_sorted(self):
+        """Prints the list sorted in ascending order."""
 
-def list():
-    pass
-def MyList(list):
+        print(f"{', '.join(str(item) for item in sorted(self))}")
