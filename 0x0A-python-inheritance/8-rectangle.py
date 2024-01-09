17f3a9f735536988e24d773100ea4f5d03f8a6de
evans-manyala <evans.manyala@gmail.com>
2024-01-09 23:11:04 +0300

    Addingn file scripts

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class Rectangle that inherits fron Base Geometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+class Rectangle (BaseGeometry):
+    """A child class of the BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """Initializning the private attributes"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.width = width
+        self.height = height
