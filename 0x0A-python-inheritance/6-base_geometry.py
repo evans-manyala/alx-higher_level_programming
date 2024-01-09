e1a2239c3545d2e778ba78926e149961fd53c90d
evans-manyala <evans.manyala@gmail.com>
2024-01-09 03:28:23 +0300

    Adding script

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a base class"""
+
+
+class BaseGeometry:
+    """Define a base class"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
