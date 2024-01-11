f9c71e761593e69b74d517e76a91aa451af0d685
evans-manyala <evans.manyala@gmail.com>
2024-01-11 05:22:23 +0300

    Adding fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,21 +7,18 @@ class Student:
     Represents a student with first name, last name, and age.
     """
 
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student
 
-def __init__(self, first_name, last_name, age):
-    """
-    Initializes a Student object with the given attributes.
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of the student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    Args:
-        first_name (str): The student's first name.
-        last_name (str): The student's last name.
-        age (int): The student's age.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-
-def to_json(self):
-    """Get a dictionary representation of the student"""
-    return self.__dict__
+    def to_json(self):
+        """Get a dictionary representation of the student"""
+        return self.__dict__
