3db7e4e956c911780a1cc14d4f86a2ce68054675
evans-manyala <evans.manyala@gmail.com>
2024-01-11 03:42:35 +0300

    Addinng script

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file (UTF-8) and
+returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and
+    returns the number of characters written.
+
+    Args:
+      filename (str, optional): The name of the file to write to.
+      Defaults to "".text (str, optional):
+      The string to write to the file. Defaults to "".
+
+    Returns:
+      int: The number of characters written to the file.
+    """
