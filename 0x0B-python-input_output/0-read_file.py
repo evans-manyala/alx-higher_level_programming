c3e1c0d29ee8f497315264cd0be7f88d7c328d95
evans-manyala <evans.manyala@gmail.com>
2024-01-11 03:25:34 +0300

    Function reads a file and prints to the stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+"""Function reads a file and prints to the stdout"""
+
+
+def read_file(filename=""):
+    """Reads the contents of a UTF-8 text file and prints it to stdout.
+
+    Args:
+        filename (str, optional): The name of the file to read. Defaults to "".
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        contents = file.read()
+        print(contents)
