b1e962cb848896a52a417ecf93b7e0de8fc2d1a9
evans-manyala <evans.manyala@gmail.com>
2024-01-11 04:03:09 +0300

    Adding script data

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function returns an object (Python data structure)
+represented by a JSON string
+"""
+
+import json
+
+
+
+def from_json_string(my_str):
+  """
+  Returns the Python object represented by a JSON string.
+
+  Args:
+    my_str: A JSON string representing a Python object.
+
+  Returns:
+    Any: The Python object represented by the JSON string, 
+    or None if the string is not valid JSON.
+  """
+  return json.loads(my_str)
