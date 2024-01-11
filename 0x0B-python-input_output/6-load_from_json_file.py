d275ac2bae5ab88ec97588d9181c69c3380e246e
evans-manyala <evans.manyala@gmail.com>
2024-01-11 04:20:31 +0300

    adding script

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that creates an Object from a 'JSON file'"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+      filename: The name of the JSON file to read.
+
+    Returns:
+      Any: The Python object represented by the JSON file, or None if the file doesn't exist or is invalid JSON.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        json_string = file.read()
+        return json.loads(json_string)
