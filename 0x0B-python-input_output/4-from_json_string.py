e28e0e0b1073aaf12d67be954cab3124df1bb0ad
evans-manyala <evans.manyala@gmail.com>
2024-01-11 04:04:18 +0300

    Fixing pycode style errors

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,16 +7,15 @@ represented by a JSON string
 import json
 
 
-
 def from_json_string(my_str):
-  """
-  Returns the Python object represented by a JSON string.
+    """
+    Returns the Python object represented by a JSON string.
 
-  Args:
-    my_str: A JSON string representing a Python object.
+    Args:
+      my_str: A JSON string representing a Python object.
 
-  Returns:
-    Any: The Python object represented by the JSON string, 
-    or None if the string is not valid JSON.
-  """
-  return json.loads(my_str)
+    Returns:
+      Any: The Python object represented by the JSON string,
+      or None if the string is not valid JSON.
+    """
+    return json.loads(my_str)
