2daf970848eff0a5c92895424cbb554414bde8ac
Evans Manyala <evans.manyala@gmail.com>
2024-01-16 07:12:24 +0300

    Adding script files requirements

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,10 +76,11 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
         f"{self.width}/{self.height}"
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0] if len(args) > 0 else self.id
-            self.width = args[1] if len(args) > 1 else self.width
-            self.height = args[2] if len(args) > 2 else self.height
-            self.x = args[3] if len(args) > 3 else self.x
-            self.y = args[4] if len(args) > 4 else self.y
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i in range(min(len(args), len(attrs))):
+                setattr(self, attrs[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
