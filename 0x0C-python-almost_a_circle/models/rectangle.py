e5d583a3eb1637b816e62c477a33870d20ff0b13
Evans Manyala <evans.manyala@gmail.com>
2024-01-16 23:15:46 +0300

    Adding features

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,13 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        # Public method to return the dictionary representation of the Rectangle
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
