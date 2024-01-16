2e3b318fd102043be3575f2565ee03e018ccd72e
Evans Manyala <evans.manyala@gmail.com>
2024-01-16 22:53:44 +0300

    Adding new script features

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,3 +20,12 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(min(len(args), len(attrs))):
+                setattr(self, attrs[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
