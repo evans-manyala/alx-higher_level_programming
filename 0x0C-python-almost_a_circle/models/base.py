2e484db336688f123e3f4572024fb1d830bd2570
evans-manyala <evans.manyala@gmail.com>
2024-01-18 13:10:04 +0300

    Adding script changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Clas Base"""
+"""Class Base"""
 
 
 import json
@@ -42,4 +42,15 @@ class Base:
     def from_json_string(json_string):
         if json_string is None or json_string == "":
             return []
-        return json.loads(json_string)
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        try:
+            with open(cls.__name__ + ".json", "r") as file:
+                list = cls.from_json_string(file.read())
+                return [cls.create(**dic) for dic in list]
+        except FileNotFoundError:
+            return []
