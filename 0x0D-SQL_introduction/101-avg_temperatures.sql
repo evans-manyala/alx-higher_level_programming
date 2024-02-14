152d73aaac647fa4ceff1148c2bcaf4d2d035813
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 00:08:29 +0300

    Adding script error fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
-SELECT city, AVG(temperature)  AS average_fahrenheit
+SELECT city, AVG(value)  AS average_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average_fahrenheit DESC;
-
+ORDER BY average_temp DESC;
