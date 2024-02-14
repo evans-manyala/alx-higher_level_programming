6b209cd458f6fd831f8a51adc28586c5ab693545
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 00:17:13 +0300

    Adding script logic

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
-
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+-- Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temperatures
-FROM temperatures
-WHERE month=7 OR month=8
+FROM temperatures WHERE month=7 OR month=8
 GROUP BY city
 ORDER BY avg_temperatures DESC
 LIMIT 3;
