becba6df2be7a080c2f8c663b3a44a45fbbdc8b6
evans-manyala <evans.manyala@gmail.com>
2024-02-15 01:46:06 +0300

    Fixinng broken code logic

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,11 @@
 -- Script to create database 'hbtn_0d_2' and user 'user_0d_2'
 -- Create user if not exists
 -- Grant SELECT privilege to the user on the database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
