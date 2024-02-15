dc92658b2809bbae445f93384aedfbb550c186f9
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 14:27:49 +0300

    Fixing more errors

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -8,5 +8,5 @@ SELECT tv_shows.title,
        SUM(tv_show_ratings.rating) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating DESC;
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC;
