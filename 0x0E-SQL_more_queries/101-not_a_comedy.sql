dbdb779e4e779a144f1623b9159258041c7d3d60
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 14:16:15 +0300

    Adding error fix to code logic

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,10 +8,9 @@
 SELECT title
 FROM tv_shows
 WHERE title NOT IN
-        ( SELECT title
+        (SELECT title
          FROM tv_shows
          INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
          INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
          WHERE tv_genres.name = 'Comedy' )
 ORDER BY title;
-
