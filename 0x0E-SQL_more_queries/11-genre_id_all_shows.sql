96bbfdebe6bce84798fc034b93defd4501838133
evans-manyala <evans.manyala@gmail.com>
2024-02-15 09:56:41 +0300

    Adding errror fixes

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -6,7 +6,6 @@
 --  The database name will be passed as an argument of the mysql command
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
+FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
