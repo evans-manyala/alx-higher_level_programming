4554fe258db1439fa01035a1d14ec037410c65fb
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 14:31:56 +0300

    Fixing persistent errors in code

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,9 +4,9 @@
 --  You can use only one SELECT statement
 --  The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title,
-       SUM(tv_show_ratings.rating) AS rating
+SELECT title,
+       SUM(tv_show_ratings.rating) AS 'rating'
 FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.id
-ORDER BY rating_sum DESC;
+LEFT JOIN tv_show_ratings ON tv_shows_ratings.show_id = tv_show.id
+GROUP BY title
+ORDER BY rating DESC;
