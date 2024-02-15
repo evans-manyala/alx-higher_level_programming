9eb1e0de8e0e47574d06ed06637d67131715816f
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 14:14:13 +0300

    Adding error fix to code logic

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,10 +3,13 @@
 --  You can use a maximum of two SELECT statements.
 --  The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title IS NULL
-    OR tv_shows.title <> 'Dexter'
-ORDER BY tv_genres.name ASC;
+WHERE name NOT IN
+        (SELECT name
+         FROM tv_genres
+         LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+         LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+         WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
