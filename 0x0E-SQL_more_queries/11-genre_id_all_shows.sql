591282533a1fb6293dedd78ce489ce1511b44120
evans-manyala <evans.manyala@gmail.com>
2024-02-15 09:54:26 +0300

    Adding script error fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -9,4 +9,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show
-ORDER BY t.title ASC, genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
