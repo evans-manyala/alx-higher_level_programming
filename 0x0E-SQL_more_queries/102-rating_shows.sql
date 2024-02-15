bc1a69249693c94556a7172d2afaffe5b1226ab2
Evans Manyala <evans.manyala@gmail.com>
2024-02-15 14:21:36 +0300

    Adding script logic

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,10 @@
+--  Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+--  Each record should display: tv_shows.title - rating sum
+--  Results must be sorted in descending order by the rating
+--  You can use only one SELECT statement
+--  The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title,
+       SUM(tv_show_ratings.rating) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
