5e69d55212b66220511281e6db6ac603d9f4a11c
Evans Manyala <evans.manyala@gmail.com>
2024-03-12 23:29:11 +0300

    Adding script

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const firstArg = process.argv[2];
+
+function factorial(n) {
+  if (isNaN(n) || n < 0) {
+    return undefined;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(firstArg);
+const result = factorial(num);
+
+if (result === undefined) {
+  console.log('Factorial is undefined for non-positive numbers and NaN');
+} else {
+  console.log(`${num}! = ${result}`);
+}
