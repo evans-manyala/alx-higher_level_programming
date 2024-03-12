6e97a9089f27e46f811aed8e28a1ffe9e7705e36
Evans Manyala <evans.manyala@gmail.com>
2024-03-13 02:21:53 +0300

    Adding more code logic

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,6 +13,17 @@ const Rectangle = class {
         console.log('X'.repeat(this.width));
       }
     }
+
+    rotate() {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
   };
   module.exports = Rectangle;
   
