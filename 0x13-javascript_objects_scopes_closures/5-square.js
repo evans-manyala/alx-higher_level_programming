c5878ec5e5122604cf657a601ae574507366f4f3
Evans Manyala <evans.manyala@gmail.com>
2024-03-13 02:48:33 +0300

    Adding error fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,37 +1,36 @@
 #!/usr/bin/node
 
 const Rectangle = class {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    rotate () {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  };
-  
-  class Square extends Rectangle {
-    constructor(size) {
-      super(size, size);
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
-  module.exports.Square = Square;
-  
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Rectangle;
+module.exports.Square = Square;
