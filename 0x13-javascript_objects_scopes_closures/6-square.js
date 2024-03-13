bb7eb2166f2139aeadbf54bc813d627a656853c8
Evans Manyala <evans.manyala@gmail.com>
2024-03-13 03:04:42 +0300

    Fixing logical errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
-#!usr/bin/node 
+#!usr/bin/node
 
 const Square = require('./5-square.js');
 const NewSquare = class extends Square {
-    charPrint (c) {
-      if (c === undefined) {
-        c = 'X';
-      }
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
-}
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
