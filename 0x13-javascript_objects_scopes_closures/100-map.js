0cf5ad49e6457f2319edc350488eddb0335b1474
Evans Manyala <evans.manyala@gmail.com>
2024-03-13 09:56:17 +0300

    Adding script error fixes

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,5 +4,5 @@ const { list } = require('./100-data.js');
 
 const newList = list.map((value, index) => value * index);
 
-console.log('Initial list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(newList);
