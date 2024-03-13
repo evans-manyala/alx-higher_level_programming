0d8035695431624d16d1c83d28894b3cbbc5e465
Evans Manyala <evans.manyala@gmail.com>
2024-03-13 03:03:48 +0300

    Adding code logic

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!usr/bin/node 
+
+const Square = require('./5-square.js');
+const NewSquare = class extends Square {
+    charPrint (c) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+}
