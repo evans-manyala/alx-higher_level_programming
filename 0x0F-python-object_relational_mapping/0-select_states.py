f713d8348b11ca769fa2b3fc9090f718d1f24309
evans-manyala <evans.manyala@gmail.com>
2024-03-15 08:27:53 +0300

    Adding more code logic

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,7 @@
 import sys
 import MySQLdb
 
+
 def list_states(username, password, database):
         try:
                 db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
@@ -15,4 +16,24 @@ def list_states(username, password, database):
                 cursor.execute("SELECT * FROM states ORDER BY id")
                 states = cursor.fetchall()
         except MySQLdb.error as e:
-                print ("Error executing query:", e)
+                print("Error executing query:", e)
+                cursor.close()
+                db.close()
+                sys.exit(1)
+        
+        for state in states
+                print(state)
+        
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+        if len(sys.argv) != 4:
+                print("Usage: python list_states.py <username> <password> <database>")
+                sys.exit(1)
+        
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        
+        list_states(username, password, database)
