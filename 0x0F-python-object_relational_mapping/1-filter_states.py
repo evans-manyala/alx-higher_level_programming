94bc634d95d48f5cf04725856938bef317d573c1
Evans Manyala <evans.manyala@gmail.com>
2024-03-22 05:23:35 +0300

    Adding script

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Script lists all states with a name starting with 'N' from
+the database `hbtn_0e_0_usa`"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username, password, database_name = sys.argv[1:]
+
+    myDB = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=database_name,
+        port=3306,
+    )
+
+    cursor = myDB.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY states.id;
+        """
+    )
+
+    states = cursor.fetchall()
+
+    for row in states:
+        print(row)
+
+    cursor.close()
+    myDB.close()
