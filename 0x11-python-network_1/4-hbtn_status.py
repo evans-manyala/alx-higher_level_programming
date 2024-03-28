bfa8b3968e16330a4d462d7b99bd5b08f1d74bba
Evans Manyala <evans.manyala@gmail.com>
2024-03-29 01:15:13 +0300

    Adding acript

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+Using the package requests to display reponse in tabulation before -
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+
+    # Define the URL to fetch
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    # Send a GET request to the URL
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+    print("\t- utf8 content: {}".format(response.text.encode("utf-8")))
