682a1e02ec29fd176ea23997a96b0223dba73ee7
Evans Manyala <evans.manyala@gmail.com>
2024-03-29 01:53:08 +0300

    Adding error fixes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,20 +6,9 @@ Using the package requests to display reponse in tabulation before -
 
 import requests
 
+
 if __name__ == "__main__":
-
-    # Define the URL to fetch
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    # Send a GET request to the URL
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Display the body of the response in the desired format
-        print("Body response:")
-        print("\t- type:", type(response.text))
-        print("\t- content:", response.text)
-    else:
-        # Print an error message if the request was not successful
-        print(f"Error fetching URL. Status code: {response.status_code}")
+    request = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
