54d5955ae69476e17d6281d18bc08ee5f5ba85cc
Evans Manyala <36915027+evans-manyala@users.noreply.github.com>
2024-03-30 15:25:07 +0300

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,9 +6,21 @@ displays the value of the variable X-Request-Id in the response header
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    url = sys.argv[1]
+# Get the URL from command-line arguments
+url = sys.argv[1]
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Check if the 'X-Request-Id' header is present in the response
+    if "X-Request-Id" in response.headers:
+        # Display the value of the 'X-Request-Id' header
+        print("X-Request-Id:", response.headers["X-Request-Id"])
+    else:
+        print("X-Request-Id header not found in the response")
+else:
+    # Print an error message if the request was not successful
+    print(f"Error fetching URL. Status code: {response.status_code}")
