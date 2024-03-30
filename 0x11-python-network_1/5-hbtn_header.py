66bd65014ad7a293cd4df7726dc2a13570250645
evans-manyala <evans.manyala@gmail.com>
2024-03-30 15:47:26 +0300

    Fixing new errors

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,11 +6,11 @@ displays the value of the variable X-Request-Id in the response header
 import requests
 import sys
 
-if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        sys.stderr.write(f"Usage: {sys.argv[0]} <URL>\n")
-        sys.exit(1)
 
+if __name__ == "__main__":
+    # Complete the URL from the command line argument
     url = sys.argv[1]
+
+    # Complete a GET request to the URL
     response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    print(response.headers.get('X-Request-Id'))
