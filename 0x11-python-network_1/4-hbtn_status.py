266be9f1ef38d08bf44fe4fd68130b7e817de05d
Evans Manyala <36915027+evans-manyala@users.noreply.github.com>
2024-03-30 15:08:09 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,11 +4,9 @@ Write a Python script that fetches https://alx-intranet.hbtn.io/status
 Using the package requests to display reponse in tabulation before -
 """
 
-import requests
-
 
 if __name__ == "__main__":
-    request = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
