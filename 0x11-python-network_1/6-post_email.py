d58a82546d08672a55c3f5c373039f3ad7886d5f
evans-manyala <evans.manyala@gmail.com>
2024-04-01 00:20:37 +0300

    Error attempt fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,6 +12,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
-response = requests.post(url, data={"email": email})
-print(response.text)
+    postmaster = {'email': email}
+    req = requests.post(url, data=postmaster)
+    print(req.text)
