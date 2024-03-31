055cbc53141954d0246d9c9ed139a88127c28351
evans-manyala <evans.manyala@gmail.com>
2024-04-01 01:25:16 +0300

    Adding 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    queue = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={"queue": queue})
+
+    try:
+        json_r = response.json()
+
+        if json_r:
+            print(f"[{json_r.get('id')}]{json_r.get('name')}")
+        else:
+            print("No results")
+    except ValueError:
+        print("Not a valid JSON")
