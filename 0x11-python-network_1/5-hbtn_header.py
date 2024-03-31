3922cc21b70c3f79e22ba9645389d1d5cfab4fc5
evans-manyala <evans.manyala@gmail.com>
2024-03-31 15:07:39 +0300

    Pycode style error fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     if (len(sys.argv) != 2):
         sys.stderr.write(f"Usage: {sys.argv[0]} <URL>\n")
         sys.exit(1)
-        
+
         # Complete the URL from the command line argument
         url = sys.argv[1]
-        
+
         # Complete a GET request to the URL
         response = requests.get(url)
         print(response.headers.get("X-Request-Id"))
