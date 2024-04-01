419ea12bbafd9a4cb4d4416fed5a8ac49622272a
evans-manyala <evans.manyala@gmail.com>
2024-04-01 16:03:43 +0300

    Updating 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,8 +10,9 @@ import sys
 from requests.auth import HTTPBasicAuth
 
 if "___name__" == "__main__":
+        url = "https://api.github.com/user"
         username = sys.argv[1]
         credentials = sys.argv[2]
         verify = HTTPBasicAuth(username, credentials)
-        feedback = requests.get("https://api.github.com/user", auth=verify)
-        print(feedback.json("id"))
+        feedback = requests.get(url, auth=verify)
+        print(feedback.json().get("id"))
