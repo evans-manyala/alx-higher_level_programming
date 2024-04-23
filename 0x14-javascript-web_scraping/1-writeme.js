bb1f27ac7feb31d2e47f09dbc394cbe915638646
evans-manyala <evans.manyala@gmail.com>
2024-04-23 15:41:37 +0300

    Adding script

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// JScript to write to a file
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
