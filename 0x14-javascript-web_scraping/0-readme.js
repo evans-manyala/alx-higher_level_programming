a4c91c8f15b4337820d51f0168bb2d4d32c93220
evans-manyala <evans.manyala@gmail.com>
2024-04-23 15:19:56 +0300

    Fixing errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,11 @@
 #!/usr/bin/node
+// JScript to read from a file
 
 const fs = require('fs');
-
-// Check if the file path argument is provided
-if (process.argv.length < 3) {
-  console.error('Please provide the file path as an argument.');
-  process.exit(1); // Exit with an error code
-}
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read the content of the file in utf-8 encoding
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
-    // Print the error object if an error occurred
-    console.error('An error occurred while reading the file:', err);
-  } else {
-    // Print the content of the file
-    console.log('File content:');
-    console.log(data);
+    console.log(err);
+    return;
   }
+  console.log(data);
 });
