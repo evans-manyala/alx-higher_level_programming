60651ff206511c4f7c5100c2cd195496ccd7989f
evans-manyala <evans.manyala@gmail.com>
2024-04-23 16:40:55 +0300

    Adding script

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+//JScript Prints the number of films with given ID
+
+const { error } = require('console');
+const request = require('request');
+let ID = 0;
+
+request.get(process.argv[2], (erro, response, body) => {
+        if (error)
+                console.log(error);
+        else
+        {
+                const content = JSON.parse(body);
+                content.results.forEach((film) => {
+                        film.characters.forEach((character) => {
+                                if (character, includes(18)){
+                                        ID = +1;
+                                }
+                        });
+                        
+                });
+                console.log(ID);
+        }
+});
