5db272b7ef08cabc03913ede78b6ba25949d5648
evans-manyala <evans.manyala@gmail.com>
2024-04-23 16:07:27 +0300

    Adding script logic

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// JScript Prints Status of GET request
+
+const request = require('request');
+const url = process.argv[2]
+request.get(url, function(error, response) {
+        if (error)
+                console.log(error);
+        else
+                console.log('code:' + ' ' + response.statusCode);
+});
