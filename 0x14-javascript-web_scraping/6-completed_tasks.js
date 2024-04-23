de3291a542159fc1cdfaaaab7cd0b5084189c30a
evans-manyala <evans.manyala@gmail.com>
2024-04-23 18:48:31 +0300

    Fixing syntax errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
-//Jscript displays the no. of tasks completed by users id
+// Jscript displays the no. of tasks completed by users id
 
 const request = require('request');
 request.get(process.argv[2], { json: true }, (error, response, body) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-      
-        const tasksCompleted = {};
-        body.forEach((todo) => {
-          if (todo.completed) {
-            if (!tasksCompleted[todo.userId]) {
-              tasksCompleted[todo.userId] = 1;
-            } else {
-              tasksCompleted[todo.userId] += 1;
-            }
-          }
-        });
-        console.log(tasksCompleted);
-      });
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
