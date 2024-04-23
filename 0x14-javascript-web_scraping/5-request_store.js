c19bf2491d9e59efbc8aedfa909a25bbb2457519
evans-manyala <evans.manyala@gmail.com>
2024-04-23 17:52:03 +0300

    Adding script

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//JScript Stores the content of a webpage in a file
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], (error, response, body) => {
+        if (error) {console.log(error)}
+        else {
+                fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+                        if (error){console.log(error);}
+                });
+        }
+});
