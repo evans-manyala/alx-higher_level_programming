502038ea86e1a3cf39b77c785e10f4ca09e88a7a
evans-manyala <evans.manyala@gmail.com>
2024-04-23 16:27:28 +0300

    Adding script logic

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// JScript uses API to get Starwars info
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) { console.log(error); } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
+});
