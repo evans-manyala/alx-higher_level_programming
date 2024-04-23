4fa2a4f29bc25cc4bbaef2ccf0a7a894550070a4
evans-manyala <evans.manyala@gmail.com>
2024-04-23 17:33:25 +0300

    Adding error fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,27 @@
 #!/usr/bin/node
-// JScript Prints the number of films with given character ID
+// JScript Prints the number of films with a given character ID
 
 const request = require('request');
+
 let filmCount = 0;
 
 request.get(process.argv[2], (error, response, body) => {
-  if (error) { console.log(error); } else {
-    const data = JSON.parse(body);
-    data.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(18)) { filmCount = +1; }
-      });
-    });
-    console.log(filmCount);
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const data = JSON.parse(body);
+  if (!data.results || !Array.isArray(data.results)) {
+    console.error('Invalid data format.');
+    return;
+  }
+  data.results.forEach((film) => {
+    if (film.characters && Array.isArray(film.characters)) {
+      if (film.characters.some((character) => character.includes('/18/'))) {
+        filmCount++;
+      }
+    }
+  });
+  console.log(filmCount);
 });
